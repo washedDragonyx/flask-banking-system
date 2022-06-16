5e0c96117b50ff1debc533a450ab739f08d1c671
Yang <j.xiang@campus.unimib.it>
2022-06-16 23:57:28 +0200

    more specific error notices, approximating to the hundredth for the amount

--- static/styles/js/scripts.js
+++ static/styles/js/scripts.js
@@ -21,9 +21,8 @@ function checkTipologiaTX(tx) {
   var receiver = tx.receiver;
   var amount = tx.amount;
 
-  if (receiver == "Withdrawal")
-  {
-    return "Withdrawal"
+  if (receiver == "Withdrawal") {
+    return "Withdrawal";
   }
   if (input != receiver) {
     return "Transfer";
@@ -34,7 +33,7 @@ function checkTipologiaTX(tx) {
 }
 
 // Funzione che, dato un array di transazioni e l'ID inserito dall'utente, inserisce
-// ogni transazione nella propria riga di tabella, omettendo il mittente e destinatario 
+// ogni transazione nella propria riga di tabella, omettendo il mittente e destinatario
 // quando l'ID inserito e l'ID della transazione sono uguali
 function populate(tx, input) {
   var table = document.getElementById("transactions-table");
@@ -47,7 +46,6 @@ function populate(tx, input) {
 
   let x = 1;
   for (let i = tx.length - 1; i >= 0; i--) {
-
     var rowNode = document.createElement("tr");
     if (i == tx.length - 1) rowNode.classList.add("bold");
     var cellNode = document.createElement("td");
@@ -71,7 +69,7 @@ function populate(tx, input) {
     rowNode.appendChild(cellNode2);
 
     if (tx[i].receiver != input) {
-      if(!(tx[i].receiver == "Withdrawal")) {
+      if (!(tx[i].receiver == "Withdrawal")) {
         var destinatario = document.createElement("div");
         temp = document.createTextNode("Receiver: " + tx[i].receiver);
         destinatario.appendChild(temp);
@@ -109,29 +107,33 @@ function httpGet(url) {
 function httpPost(url, arr) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open("POST", url, true);
-  xmlHttp.setRequestHeader('Content-Type', 'application/json');
+  xmlHttp.setRequestHeader("Content-Type", "application/json");
 
-  var result = ""
-  xmlHttp.onreadystatechange = (function(_this) {
-    return function() {
+  var result = "";
+  xmlHttp.onreadystatechange = (function (_this) {
+    return function () {
       if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
         result = xmlHttp.responseText;
-        var parsed = JSON.parse(result)
-        if(parsed.Status == "Success") {
-          resultPOST("The transaction was completed successfully!", parsed.Status);
-        }
-        else if(parsed.Status == "Failure") {
+        var parsed = JSON.parse(result);
+        if (parsed.Status == "Success") {
+          resultPOST(
+            "The transaction was completed successfully!",
+            parsed.Status
+          );
+        } else if (parsed.Status == "Failure") {
           resultPOST(parsed.Error, parsed.Status);
         }
-      };
-    }
+      }
+    };
   })(this);
 
-  xmlHttp.send(JSON.stringify({
-    from: arr[0],
-    to: arr[1],
-    amount: arr[2]
-  }));
+  xmlHttp.send(
+    JSON.stringify({
+      from: arr[0],
+      to: arr[1],
+      amount: arr[2],
+    })
+  );
 }
 
 function resultPOST(txt, result) {
@@ -139,30 +141,27 @@ function resultPOST(txt, result) {
   var alert = document.getElementById("alert");
   var title = document.getElementById("title");
   alert.innerHTML = txt;
-  if (result == "Success")
-  {
-    title.innerHTML = "All done! 👍🏻"
+  if (result == "Success") {
+    title.innerHTML = "All done! 👍🏻";
+  } else if (result == "Failure") {
+    title.innerHTML = "Something went wrong! 🤔";
   }
-  else if (result == "Failure")
-  {
-    title.innerHTML = "Something went wrong! 🤔"
-  }
-  flipCard("flip-card")
+  flipCard("flip-card");
   // flip_card.classList.add("flip-card-flip");
 }
 
 function flipCard(id) {
-  var flip_card = document.getElementById(id)
-  flip_card.classList.toggle("flip-card-flip")
+  var flip_card = document.getElementById(id);
+  flip_card.classList.toggle("flip-card-flip");
 }
 
 function resetTransfer() {
-  var sender = document.getElementById("input-id-sender")
-  var receiver = document.getElementById("input-id-receiver")
-  var amount = document.getElementById("input-amount")
-  sender.value = ""
-  receiver.value = ""
-  amount.value = ""
+  var sender = document.getElementById("input-id-sender");
+  var receiver = document.getElementById("input-id-receiver");
+  var amount = document.getElementById("input-amount");
+  sender.value = "";
+  receiver.value = "";
+  amount.value = "";
 }
 
 // Chiamato dal pulsante "Cerca", utilizza l'ID inserito per ottenere tutti i dati del
@@ -185,8 +184,8 @@ function getInput() {
       var name = document.getElementById("acc-name");
       var surname = document.getElementById("acc-surname");
 
-      var tempText = "Account " + parsed.AccountID + " found ✅"
-      showNotice(tempText, "notice", "green")
+      var tempText = "Account " + parsed.AccountID + " found ✅";
+      showNotice(tempText, "notice", "green");
 
       id.innerHTML = "";
       balance.innerHTML = "";
@@ -211,15 +210,10 @@ function getInput() {
 
       populate(parsed.Transactions, input);
       show();
-    }
-    else if (parsed.Status == "Failure") {
+    } else if (parsed.Status == "Failure") {
       hide();
-      var tempText = parsed.Error + " ❌"
-      showNotice(
-        tempText,
-        "notice",
-        "red"
-      );
+      var tempText = parsed.Error + " ❌";
+      showNotice(tempText, "notice", "red");
     }
   }
 }
@@ -245,7 +239,10 @@ function setEnter() {
 // un bordo rosso al textbox di input e mostra un avviso di errore
 function checkLength(input_id, notice_id) {
   var input = document.getElementById(input_id);
-  if (input.value.length != 20) {
+  if (input.value.length == 0) {
+    input.classList.add("border-red");
+    showNotice("❗The ID can't be empty❗", notice_id, "red");
+  } else if (input.value.length != 20) {
     input.classList.add("border-red");
     showNotice(
       "❗The entered ID's length must be 20 characters❗",
@@ -266,16 +263,15 @@ function showNotice(notice, notice_id, color) {
   var notice_box = document.getElementById(notice_id);
   notice_box.innerHTML = notice;
   notice_box.classList.remove("op0");
-  if(notice_box.classList.contains("red") && color == "green"){
-    notice_box.classList.remove("red")
-    notice_box.classList.add("green")
-    setTimeout(function() {
+  if (notice_box.classList.contains("red") && color == "green") {
+    notice_box.classList.remove("red");
+    notice_box.classList.add("green");
+    setTimeout(function () {
       hideNotice(notice_id);
     }, 5000);
-  }
-  else if(notice_box.classList.contains("green") && color == "red"){
-    notice_box.classList.remove("green")
-    notice_box.classList.add("red")
+  } else if (notice_box.classList.contains("green") && color == "red") {
+    notice_box.classList.remove("green");
+    notice_box.classList.add("red");
   }
 }
 
@@ -295,39 +291,59 @@ function getPageName() {
 
 // Invia al sistema i dati mittente, destinatario e ammontare
 function sendTransfer() {
-  checkLength("input-id-sender", "error-sender")
-  checkLength("input-id-receiver", "error-receiver")
-  checkPositive("input-amount", "error-amount")
-  if(
-  (checkLength("input-id-sender", "error-sender")) &&
-  (checkLength("input-id-receiver", "error-receiver")) &&
-  (checkPositive("input-amount", "error-amount")))
-  {
+  checkLength("input-id-sender", "error-sender");
+  checkLength("input-id-receiver", "error-receiver");
+  checkAmount("input-amount", "error-amount");
+  if (
+    checkLength("input-id-sender", "error-sender") &&
+    checkLength("input-id-receiver", "error-receiver") &&
+    checkAmount("input-amount", "error-amount")
+  ) {
     var arr = [
       document.getElementById("input-id-sender").value,
       document.getElementById("input-id-receiver").value,
-      document.getElementById("input-amount").value
-    ]
+      roundToHundredth(document.getElementById("input-amount").value)
+    ];
     httpPost("http://127.0.0.1:5000/api/transfer", arr);
   }
 }
 
-function checkPositive(input_id, notice_id) {
-  var input = document.getElementById(input_id)
-  if(input.value > 0){
-    input.classList.remove("border-red");
-    hideNotice(notice_id)
-    return true}
-  else
-  {
+// Controllo il valore inserito in ammontare (Amount)
+function checkAmount(input_id, notice_id) {
+  var input = document.getElementById(input_id);
+  console.clear()
+  // console.log("full value: " + input.value)
+  // console.log("millesimo: " + input.value[4])
+  
+
+  if (input.value == "") {
     input.classList.add("border-red");
-    showNotice(
-      "❗The amount must be higher than zero❗",
-      notice_id,
-      "red"
-    )
-    return false
+    showNotice("❗The amount can't be empty❗", notice_id, "red");
+    return false;
+  }
+
+  if (input.value == 0) {
+    input.classList.add("border-red");
+    showNotice("❗The amount can't be zero❗", notice_id, "red");
+    return false;
+  }
+
+  if (input.value < 0.01) {
+    input.classList.add("border-red");
+    showNotice("❗The amount must be higher than 0.01❗", notice_id, "red");
+    return false;
+  }
+
+  if (input.value > 0) {
+    input.classList.remove("border-red");
+    hideNotice(notice_id);
+    return true;
   }
 }
+
+function roundToHundredth(value) {
+  console.log(value)
+  return Number.parseFloat(value).toFixed(2);
+};
 
 if (getPageName() == "") setEnter();
